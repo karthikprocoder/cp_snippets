6fbfdabdf5c188f94aab37f0776c8ffb06283d99
Karthik <katy@pop-os.localdomain>
2023-10-02 12:48:43 +0530

    added split method in string_methods.cpp

--- snippets/general/string_methods.cpp
+++ snippets/general/string_methods.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool is_palindrome(string& str) {
-    for(int i = 0; i < str.size() / 2; i++) {
+    for(size_t i = 0; i < str.size() / 2; i++) {
         if(str[i] != str[str.size() - i - 1]) return false;
      }
      return true;
@@ -10,13 +10,26 @@ bool is_palindrome(string& str) {
 
 bool is_vowel(char ch) {
     string v = "aeiouAEIOU";
-    return v.find(ch) != -1;
+    return v.find(ch) != (size_t)-1;
 }
 
 string to_lowercase(string& s) {
-    for(int i = 0; i < s.size(); i++) 
+    for(size_t i = 0; i < s.size(); i++) 
         if(s[i] <= 'Z') s[i] += 32;
     return s;
+}
+
+vector<string> split(string& s) {
+    // spilt by spaces
+    vector<string> words;
+    for(size_t i = 0, j = 0; i < s.size(); i = j+1) {
+        string word = "";
+        for(j = i; j < s.size() && s[j] != ' '; j++) {
+            word += s[j];
+        }
+        words.emplace_back(word);
+    }
+    return words;
 }
 
 void ascii_value(char ch) {
@@ -27,6 +40,10 @@ void ascii_value(char ch) {
 }
 
 int main() {
-
-
+    string s;
+    getline(cin, s);
+    vector<string> words = split(s);
+    for(auto w : words) {
+        cout << w.size() << "   " << w << "\n";
+    }
 }
