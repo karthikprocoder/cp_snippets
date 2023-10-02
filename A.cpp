0a0b9321d302e18a4e051f4a790d0e8df5464624
Karthik <katy@pop-os.localdomain>
2023-10-02 11:21:22 +0530

    created inc, snippets folders and added temp.cpp

---
+++ A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "inc/debug.h"
+#else
+#define dbg(...) 1
+#endif
+
+/*
+
+*/
+
+void solve(int test_no) {
+    int N;
+    cin >> N;
+    dbg(N);
+    cout << N * N << '\n';
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    for(int t = 1; t <= T; t++) {
+        solve(t);
+    }
+}
---
+++ snippets/temp.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "inc/debug.h"
+#else
+#define dbg(...) 1
+#endif
+
+/*
+
+*/
+
+void solve(int test_no) {
+    
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tt = 1;
+    cin >> tt;
+    for(int t = 1; t <= tt; t++) {
+        solve(t);
+    }
+}
